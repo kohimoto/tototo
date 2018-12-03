c801e2de8b5f5c357fbf251d059f415ce2b1fc76
kohimoto1! <kohimotoj@gmail.com>
2018-12-03 18:18:07 +0900

    add sass

--- app/public/web/wp-content/themes/twentyseventeen-child/functions.php
+++ app/public/web/wp-content/themes/twentyseventeen-child/functions.php
@@ -5,7 +5,7 @@ function theme_enqueue_scripts() {
   wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/assets/js/jquery.min.js' );
   wp_enqueue_script( 'child-slick-script', get_stylesheet_directory_uri() . '/assets/js/jquery.infinitescroll.min.js' );
   wp_enqueue_script( 'child-custom-script', get_stylesheet_directory_uri() . '/assets/js/custom.js' );
-  wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/style.css', array('parent-style')
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/assets/css/style.css', array('parent-style')
 );
 }
 ?>
