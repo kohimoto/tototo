b99456499edfadde6cedf4db86d9ebadc9f14d33
kohimoto1! <kohimotoj@gmail.com>
2018-12-03 18:07:33 +0900

    theme remove

--- app/public/web/wp-content/themes/alchem-child/content-home.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-<?php
-  get_template_part('home-sections/section','news');
-?>
-</div>
--- app/public/web/wp-content/themes/alchem/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" class="search-form" action="<?php echo esc_url(home_url('/'));?>">
- <div>
-  <label class="sr-only"><?php _e("Search for","alchem");?>:</label>
-   <input type="text" name="s" value="" placeholder="<?php _e("Search...","alchem");?>">
-   <input type="submit" value="">
-  </div>
- </form>
