14f426194d96ac0754d7a8d9071b9dbf5cf28779
kohimoto1! <kohimotoj@gmail.com>
2018-12-05 10:34:39 +0900

    masory

--- app/public/web/wp-content/themes/twentyseventeen-child/functions.php
+++ app/public/web/wp-content/themes/twentyseventeen-child/functions.php
@@ -3,9 +3,9 @@ add_action('wp_enqueue_scripts','theme_enqueue_scripts' );
 function theme_enqueue_scripts() {
   wp_enqueue_style('parent-style', get_template_directory_uri() .'/style.css' );
   wp_enqueue_script( 'child-script', get_stylesheet_directory_uri() . '/assets/js/jquery.min.js' );
-  wp_enqueue_script( 'child-slick-script-masonry', get_stylesheet_directory_uri() . '/assets/js/jquery.masonry.min.js' );
-  wp_enqueue_script( 'child-slick-script-infinitescroll', get_stylesheet_directory_uri() . '/assets/js/jquery.infinitescroll.min.js' );
-  wp_enqueue_script( 'child-custom-script', get_stylesheet_directory_uri() . '/assets/js/custom.js' );
+  wp_enqueue_script( 'child-script-masonry', get_stylesheet_directory_uri() . '/assets/js/jquery.masonry.min.js' );
+  wp_enqueue_script( 'child-script-infinitescroll', get_stylesheet_directory_uri() . '/assets/js/jquery.infinitescroll.min.js' );
+  wp_enqueue_script( 'child-script-custom', get_stylesheet_directory_uri() . '/assets/js/custom.js' );
   wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/assets/css/style.css', array('parent-style')
 );
 }
