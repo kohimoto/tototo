6929c65dabf82eaf8bd2b2f968b4fbfb9150a954
kohimoto <kohimotoj@gmail.com>
2018-12-05 17:34:20 +0900

    font/header

--- app/public/web/wp-content/themes/twentyseventeen-child/header.php
+++ app/public/web/wp-content/themes/twentyseventeen-child/header.php
@@ -18,8 +18,18 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
 <?php wp_head(); ?>
+<link href="https://fonts.googleapis.com/css?family=Khula:800&amp;subset=devanagari,latin-ext" rel="stylesheet">
+<script>
+  (function(d) {
+    var config = {
+      kitId: 'bzs1sru',
+      scriptTimeout: 3000,
+      async: true
+    },
+    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+</script>
 </head>
 
 <body <?php body_class(); ?>>
