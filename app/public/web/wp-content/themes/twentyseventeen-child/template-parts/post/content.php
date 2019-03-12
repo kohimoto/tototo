ec8097bdcbf841995620e8cddceea1adc50e59bd
kohimoto1! <kohimotoj@gmail.com>
2019-03-12 15:16:20 +0900

    topはバルーン、カテゴリページは一覧

--- app/public/web/wp-content/themes/twentyseventeen-child/template-parts/post/content.php
+++ app/public/web/wp-content/themes/twentyseventeen-child/template-parts/post/content.php
@@ -11,15 +11,38 @@
  */
 
 ?>
-      <header class="entry-header">
-        <p class="detail-date"><?php echo get_the_date('Y/m/d'); ?></p>
-        <?php the_title( '<h2 class="entry-title"><span class="mask angle entry-title-list">', '</span></h2>' ); ?>
-      </header><!-- .entry-header -->
-      <div class="entry-detail-content">
-      <?php
-      the_content( sprintf(
-        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-        get_the_title()
-        ) );
-        ?>
-      </div>
+
+	<?php
+	if ( is_sticky() && is_home() ) :
+		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
+	endif;
+	?>
+  <?php
+  //topとcategoryページでは記事タイトルは表示しない
+  ?>
+  	<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+<?php
+$cnt++;
+$cnt_arr = array("2","9","11","12","16","17");
+//$cnt_arr = array("2","8","10","11","15","16");
+$add_class = "";
+if(in_array($cnt, $cnt_arr)) {
+  $add_class = "circle_big";
+}
+  $check_cnt = "circle_no_".$cnt;
+?>
+
+		<div class="post-thumbnail grid-item circle-disp <?php echo $add_class;?> <?php echo $check_cnt;?>">
+      <div class="item-content">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+			</a>
+    </div>
+		</div>
+	<?php endif; ?>
+
+	<?php
+	if ( is_single() ) {
+		twentyseventeen_entry_footer();
+	}
+	?>
